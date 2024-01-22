16317dd1e63a0812f9f19f7fc41b64c6732396a1
Exa-qvq <exaqvq@gmail.com>
2024-01-23 01:16:51 +0800

    upgrade

--- functions/embed_content.py
+++ functions/embed_content.py
@@ -5,23 +5,22 @@ from functions.steam import *
 from functions.kreedz import *
 from functions.date_time import *
 
+
 def user_info(discord_id) -> discord.Embed:
 
     steamid = retrieve_steam_id(discord_id)
     steamid64 = convert_steamid_to_steamid64(steamid)
 
     name = retrieve_user_name(steamid)
-    joindate = formatted_datetime(retrieve_join_date(steamid))
-    lastseen = formatted_datetime(retrieve_last_seen(steamid))
-    jd_timestamp = joindate.timestamp()
-    lastseen_ts = lastseen.timestamp()
+    joindate = format_string_to_datetime(retrieve_join_date(steamid))
+    lastseen = format_string_to_datetime(retrieve_last_seen(steamid))
     pfp_url = get_steam_pfp(steamid64)
     profile_url = get_steam_profile_url(steamid64)
     kzgoeu_url = get_kzgoeu_profile_url(steamid)
 
     embed = Embed(
         title="Info",
-        description=f"First join: <t:{jd_timestamp}>\nLast seen: <t:{lastseen_ts}>",
+        description=f"First join: <t:{joindate.timestamp()}>\nLast seen: <t:{lastseen.timestamp()}>",
         colour=discord.Colour.green()
     )
     embed.set_author(name=name, icon_url=pfp_url, url=profile_url)
