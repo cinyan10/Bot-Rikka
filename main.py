804528fc9d6868628c08429c5ccedb8c2c6bb4cb
Exa-qvq <exaqvq@gmail.com>
2024-01-24 03:53:06 +0800

    upgrade

--- main.py
+++ main.py
@@ -14,21 +14,8 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
-# Define 'load' as an async function
-async def load():
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py') and not filename.startswith('__'):
-            cog_name = f'cogs.{filename[:-3]}'
-            try:
-                await bot.load_extension(cog_name)
-            except commands.ExtensionFailed as e:
-                print(f'Failed to load extension {cog_name}: {e}')
-
-
-async def main():
-    await load()
-    await bot.start(TOKEN)
-
-if __name__ == '__main__':
-    asyncio.run(main())
+bot.run(TOKEN)
