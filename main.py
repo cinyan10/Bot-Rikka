2d22043458306e1bde8a5141eab38e463b1bc9ab
Exa-qvq <exaqvq@gmail.com>
2024-01-24 03:36:45 +0800

    upgrade

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
-import os
-import discord
 from discord.ext import commands
-import asyncio
 from functions.gokzcn import *
 from config import *
+import asyncio
 
 # Initialize bot
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Load cogs
-initial_extensions = [f'cogs.{filename[:-3]}' for filename in os.listdir('./cogs') if filename.endswith('.py')]
 
-
-async def load_cogs():
-    for extension in initial_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            print(f'Failed to load extension {extension}: {str(e)}')
+async def load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
 
 async def main():
-    await load_cogs()
+    await load()
     await bot.start(TOKEN)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
