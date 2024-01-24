f605ff8ec4abcabe8d4341353c02f10c635e7185
Exa-qvq <exaqvq@gmail.com>
2024-01-24 13:21:48 +0800

    change main.py to run without async

--- main.py
+++ main.py
@@ -15,14 +15,11 @@ async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
 
-async def load():
+def load_cogs():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{filename[:-3]}')
+            bot.load_extension(f'cogs.{filename[:-3]}')
 
 
-async def main():
-    await load()
-    await bot.start(TOKEN)
-
-asyncio.run(main())
+load_cogs()
+bot.run(TOKEN)
