2bc9f86c2f7fd1c6de0d6e58575db9996a6cafee
Exa-qvq <exaqvq@gmail.com>
2024-01-24 15:01:24 +0800

    test setup_logging

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 from config import TOKEN
 import asyncio
 
+discord.utils.setup_logging()
+
 # Initialize bot
 intents = discord.Intents.default()
 intents.message_content = True
