d1fa00fb4f3cb1ceb24df6be026846ae1c6dfe35
Exa-qvq <exaqvq@gmail.com>
2024-01-29 02:48:15 +0800

    Re structure

--- dc_utils/whitelist.py
+++ dc_utils/whitelist.py
@@ -14,39 +14,40 @@ from functions.steam.steam_user import check_steam_bans
 async def get_whitelisted(ctx):
     user_id = ctx.author.id
     steamid = discord_id_to_steamid(user_id)
+    print(steamid)
     steamid64 = convert_steamid(steamid, 'steamid64')
+    print(steamid64)
 
     # Check if the player has been banned by VAC or multiple games
     ban_status = check_steam_bans(steamid64)
     if ban_status['vac_banned']:
-        return await ctx.send(embed=Embed(title="You have been banned by VAC!!!", colour=discord.Colour.red()))
+        await ctx.send(embed=Embed(title="You have been banned by VAC!!!", colour=discord.Colour.red()))
+        return
     elif ban_status['game_ban_count'] > 1:
-        return await ctx.send(embed=Embed(title=f"You have been banned by {ban_status['game_ban_count']} games!!!", colour=discord.Colour.red()))
+        await ctx.send(embed=Embed(title=f"You have been banned by {ban_status['game_ban_count']} games!!!", colour=discord.Colour.red()))
+        return
     else:
         await ctx.send(embed=Embed(title=f"You haven't been banned", colour=discord.Colour.green()))
 
     # Check if the player is in steam group
     is_group = is_in_group(steamid64)
 
-    await asyncio.sleep(2)
-
     print(is_group)
     if is_group:
         await ctx.send(embed=Embed(title=f"You're In the Steam Group", colour=discord.Colour.green()))
     else:
         await ctx.send(embed=Embed(title=f"You haven't join in Steam Group yet!", colour=discord.Colour.red()))
+        return
 
-    await asyncio.sleep(2)
-
-    # Check if the player got enough pts
+        # Check if the player got enough pts
     for i in range(3):
         stats = KzGlobalStats(steamid64, i)
+        print(i)
         if stats.is_reach_pts():
             await ctx.send(embed=Embed(title=f"You reached 50k pts!!", colour=discord.Colour.green()))
             update_whitelist_status(steamid)
             await ctx.send(embed=Embed(title=f"Added you to the whitelist", colour=discord.Colour.green()))
-            await set_wl_role(ctx, steamid64)
+            await set_wl_role(ctx, steamid)
             break
-        await asyncio.sleep(2)
 
     await ctx.send(embed=Embed(title=f"You Didn't reach 50k pts!!", colour=discord.Colour.red()))
